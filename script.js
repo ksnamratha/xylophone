ac42c294448e536e85c0f6a8ac48e8dcd12eafd2
ksnamratha <48938020+ksnamratha@users.noreply.github.com>
2020-06-22 17:04:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var x = document.getElementById("A"); 
+
+function audio() { 
+   x.play(); 
+                 }
+  function audio() { 
+   x.pause(); 
+               }
+ if (e.keyCode == 65) {      
+     if(document.getElementById("A").paused){
+     document.getElementById('audio').play();
+    }
+    else{
+     document.getElementById("A").pause();
+   }
+ };
